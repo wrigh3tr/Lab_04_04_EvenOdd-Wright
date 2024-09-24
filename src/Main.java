d4e4050b4039552d374f5bd386a00cd85d889b87
Tyler Wright <tylerwright@MacBook-Air.local>
2024-09-23 22:16:27 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+    int numToExamine = 2;
+    if (numToExamine % 2 == 0) System.out.println("Number is Even ");else
+        {
+            System.out.println("Number is Odd ");
+        }
+
+    }
+}
